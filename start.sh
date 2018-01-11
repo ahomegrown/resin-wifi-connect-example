22de1b8654049d7c6f62962090391e85e8992b95
Andrew Home <andrew.home.lee@gmail.com>
2018-01-11 15:06:57 -0800

    finally git it

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run one process loop
-python src/process.py
+# python src/process.py
 
 # Start resin-wifi-connect
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
